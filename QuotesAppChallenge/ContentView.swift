041f2204d47f2b5bb0a079416aa3001691eb2f04
hoeppken <rafael_rsv@hotmail.com>
2025-07-02 07:11:29 +0200

    Initial Commit

---
+++ QuotesAppChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuotesAppChallenge
+//
+//  Created by Rafael dos Santos Varela on 02.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuotesAppChallenge/QuotesAppChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppChallengeApp.swift
+//  QuotesAppChallenge
+//
+//  Created by Rafael dos Santos Varela on 02.07.25.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
